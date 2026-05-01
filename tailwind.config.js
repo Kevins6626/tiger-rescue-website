8354888477d41cbb306aa65e783e0cef4f8a6197
Kevin <kevin.sakowicz@dmz.devlnk.net>
2026-05-01 16:54:36 -0400

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,59 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import plugin from 'tailwindcss/plugin';
+import typographyPlugin from '@tailwindcss/typography';
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--aw-color-primary)',
+        secondary: 'var(--aw-color-secondary)',
+        accent: 'var(--aw-color-accent)',
+        default: 'var(--aw-color-text-default)',
+        muted: 'var(--aw-color-text-muted)',
+      },
+      fontFamily: {
+        sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+        serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
+        heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+      },
+
+      animation: {
+        fade: 'fadeInUp 1s both',
+        heartBeat: 'heartBeat 1s ease-in-out infinite',
+      },
+
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateY(2rem)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        heartBeat: {
+          '0%': {
+            transform: 'scale(1)',
+          },
+          '14%': {
+            transform: 'scale(1.2)',
+          },
+          '28%': {
+            transform: 'scale(1)',
+          },
+          '42%': {
+            transform: 'scale(1.2)',
+          },
+          '70%': {
+            transform: 'scale(1)',
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    typographyPlugin,
+    plugin(({ addVariant }) => {
+      addVariant('intersect', '&:not([no-intersect])');
+    }),
+  ],
+  darkMode: 'class',
+};
